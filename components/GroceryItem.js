e63898fdb550d5b7f01e8085f3bdb1318565a489
ASETML <alban.segalen@eduvaud.ch>
2025-06-02 10:53:42 +0200

    Récupération des charactères fonctionnelle

--- components/GroceryItem.js
+++
@@ -1,9 +0,0 @@
-app.component("grocery-item", {
-  props: ["item", "index"],
-  template: /*html*/ `<div class="item"><p>{{item}}</p><p>&nbsp;- </p><p class="del" @click="removeItem">❌</p></div> `,
-  methods: {
-    removeItem() {
-      this.$emit("remove-item", this.index);
-    },
-  },
-});
--- main.js
+++ main.js
@@ -1,19 +1,59 @@
 const app = Vue.createApp({
   data() {
     return {
-      items: [],
-      item: "",
+      letter: "",
+      guessedLetters: [],
+      word: "dotnet maui",
+      mistakes: 0,
+      guessedWord: "",
     };
   },
   methods: {
-    addToList() {
-		if (this.item != "") {
-			this.items.push(this.item);
-			this.item = "";
-		}
+    startGame() {
+      this.guessedWord = ""
+      this.guessedLetters = []
+      this.mistakes = 0
+      this.fillWord()
     },
-    removeItem(index) {
-      this.items.splice(index, 1);
+    fillWord() {
+      for (let i = 0; i < this.word.length; i++) {
+        this.guessedWord += "_"
+      }
     },
+    guessLetter() {
+      if (this.letter.match(/[A-Za-z\-\s]/) && !this.guessedLetters.includes(this.letter)) {
+        this.guessedLetters.push(this.letter)
+        
+        console.log(this.guessedLetters)
+        this.guessWord(this.letter)
+        this.letter = "";
+      }
+    },
+    guessWord(letter) {
+      if (this.word.includes(letter)) {
+        //find every index of guessed letter in word
+        let indices = [];
+        for(let i = 0; i < this.word.length; i++) {
+            if (this.word[i] === letter) {
+              indices.push(i)
+            };
+        }
+
+        //replace
+        for (const i of indices) {
+          let firstPart = this.guessedWord.substr(0, i);
+          let lastPart = this.guessedWord.substr(i + 1);
+            
+          this.guessedWord = firstPart + letter + lastPart; 
+        }
+        
+        console.log(this.guessedWord)
+
+      }
+      else {
+        this.mistakes++
+        console.log(this.mistakes)
+      }
+    }
   },
 });
